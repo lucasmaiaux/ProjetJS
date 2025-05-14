feba81135867c189fe20d5c4dec0e47452dc93cb
Lucas <lucas.maiaux@le-campus-numerique.fr>
2025-05-14 16:13:10 +0200

    [WIP] memory

--- memory.js
+++ memory.js
@@ -1,5 +1,14 @@
 const main_feed = document.querySelector(".main-feed")
-
+const settings_wrap = document.querySelector(".settings_wrap")
+const memory_wrap = document.querySelector(".memory_wrap")
+const gameParameters = {
+    name: '',
+    difficulty: '',
+    id: '',
+    pairs_to_win: '',
+    max_tries: '',
+    images: []
+}
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -20,15 +29,15 @@ window.onclick = function (event) {
     }
 }
 
-// Fonction pour lancer une partie
-async function showParameters(difficulty) {
-    const old_info = main_feed.querySelector(".info_game")
-    if (old_info) main_feed.removeChild(old_info)
+// Récuperer les paramètres en fonction de la difficulté
+async function updateParameters(difficulty) {
+    const old_info = settings_wrap.querySelector(".info_game")
+    if (old_info) settings_wrap.removeChild(old_info)
 
     let api = ""
-    if (difficulty === 0) api = "https://mocki.io/v1/14445d57-d5ff-40ef-97e7-6fddd6732b43"
-    else if (difficulty === 1) api = "https://mocki.io/v1/efd56d56-f8fc-4fa3-bfd6-41604aecda4c"
-    else if (difficulty === 2) api = "https://mocki.io/v1/4713d124-aedf-4001-84df-7db19c18f70a"
+    if (difficulty === 0) api = "https://mocki.io/v1/8e5e6c35-8c52-4935-89cf-1dca5b18a02f"
+    else if (difficulty === 1) api = "https://mocki.io/v1/ae626cf9-8588-4a45-9568-ef60240aa698"
+    else if (difficulty === 2) api = "https://mocki.io/v1/8989c7ca-f24c-4db0-9861-ad65faa42032"
 
     const reponse = await fetch(api)
     data = await reponse.json()
@@ -42,8 +51,26 @@ async function showParameters(difficulty) {
     <p><b>Paires pour gagner</b> : ${data.pairs_to_win}</p>
     <p><b>Tentatives max</b>: ${data.max_tries}</p>`
 
-    main_feed.appendChild(info_game)
+    gameParameters.name = data.name
+    gameParameters.difficulty = data.difficulty
+    gameParameters.id = data.id
+    gameParameters.pairs_to_win = data.pairs_to_win
+    gameParameters.max_tries = data.max_tries
+    gameParameters.images = data.images
+
+
+    const btn_startGame = document.createElement("button")
+    btn_startGame.classList.add("btn-startGame")
+    btn_startGame.innerText = "Jouer"
+    btn_startGame.addEventListener('click', () => {
+        initGame()
+    });
+
+    info_game.appendChild(btn_startGame)
+    settings_wrap.appendChild(info_game)
+
     //console.log(data)
+    console.log(gameParameters)
 }
 
 // On attribue chaque difficulté à chaque bouton
@@ -52,13 +79,124 @@ const selector_medium = document.querySelector("#myDropdown_medium")
 const selector_hard = document.querySelector("#myDropdown_hard")
 
 selector_easy.addEventListener('click', () => {
-    showParameters(parseInt(0))
+    updateParameters(0)
 })
 
 selector_medium.addEventListener('click', () => {
-    showParameters(parseInt(1))
+    updateParameters(1)
 })
 
 selector_hard.addEventListener('click', () => {
-    showParameters(parseInt(2))
+    updateParameters(2)
 })
+
+// MEMORY
+let cards = []
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+
+// Mélange Fisher–Yates
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function initGame() {
+    //console.log("START")
+
+    // Mélange des cartes
+    cards = shuffle([...gameParameters.images, ...gameParameters.images])
+    console.log(cards)
+
+    // Rendu plateau
+    /*
+    const cells = memory_wrap.querySelectorAll(".memory_cell")
+    cells.forEach(function (cell) {
+        const cell_img = document.createElement("img")
+        cell_img.classList.add("back")
+        cell_img.src = "images/memory/Memory_image_1_128x128.png"
+        //console.log(cell_img)
+        cell.addEventListener('click', () => {
+            //console.log("Test")
+            cell_img.classList.toggle("invisible")
+        })
+        cell.appendChild(cell_img)
+    })
+    */
+
+    const board = document.querySelector('#memory-board');
+    board.innerHTML = '';
+
+    cards.forEach((val, index) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.value = val.url;
+        card.dataset.index = index;
+
+        // Remplissage 2 faces
+        card.innerHTML = `<div class="front hidden"><img src="${card.dataset.value}"></img></div><div class="back"><img src="images/memory/Memory_back.png"></img></div>`;
+
+        card.addEventListener('click', flipCard);
+        board.appendChild(card);
+    });
+}
+
+// Gestion du clic
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flipped');
+    this.querySelector('.front').classList.toggle("hidden")
+    this.querySelector('.back').classList.toggle("hidden")
+
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    lockBoard = true;
+    checkForMatch();
+}
+
+// Vérification de la paire
+function checkForMatch() {
+    const isMatch = firstCard.dataset.value === secondCard.dataset.value;
+
+    if (isMatch) {
+        disableCards();
+    } else {
+        unflipCards();
+    }
+}
+
+// Les cartes restent retournées
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+// Les cartes se retournent face cachée après un délai
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipped');
+        firstCard.querySelector('.front').classList.toggle("hidden")
+        firstCard.querySelector('.back').classList.toggle("hidden")
+        secondCard.querySelector('.front').classList.toggle("hidden")
+        secondCard.querySelector('.back').classList.toggle("hidden")
+        resetBoard();
+    }, 1000);
+}
+
+// Réinitialisation des variables
+function resetBoard() {
+    [firstCard, secondCard] = [null, null];
+    lockBoard = false;
+}
